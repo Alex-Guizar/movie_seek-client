87089831e8f5dab0fb19d44476d9677763daeb4a
Alex Guizar <alexguizar90@gmail.com>
2021-09-17 17:30:54 -0700

    Added prop-types, register handler, and scss file import to the login-view module

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './login-view.scss';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -8,21 +11,35 @@ export function LoginView(props) {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call this.props.onLoggedIn(username) */
-    /* this.props.onLoggedIn(username) */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  }
+
+  /** Set view to registration */
+  const handleRegister = (e) => {
+    e.preventDefault();
+    props.setRegister();
   }
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
+    <React.Fragment>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <button type="button" onClick={handleSubmit}>Submit</button>
+      </form>
+      <p><a href="#" onClick={handleRegister}>Don't have an account? Click here to register!</a></p>
+    </React.Fragment>
   );
 }
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired,
+  setRegister: PropTypes.func.isRequired
+};
