70b16939d6389a25fc3ba04379e3ffada5e02320
Alex Guizar <alexguizar90@gmail.com>
2021-09-23 22:09:48 -0700

    Added control ids to the form groups

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -21,22 +21,22 @@ export function RegistrationView(props) {
 
   return (
     <Form>
-      <Form.Group>
+      <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
-      <Form.Group>
+      <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
         <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
       </Form.Group>
 
-      <Form.Group>
+      <Form.Group controlId="formEmail">
         <Form.Label>Email:</Form.Label>
         <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
       </Form.Group>
 
-      <Form.Group>
+      <Form.Group controlId="formBirthday">
         <Form.Label>Birth Date:</Form.Label>
         <Form.Control type="date" onChange={e => setBirthdate(e.target.value)} />
       </Form.Group>
