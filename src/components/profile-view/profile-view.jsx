3a7a8a29547f39a77de54616112b7ce3ba70fb95
Alex Guizar <alexguizar90@gmail.com>
2021-09-28 19:58:23 -0700

    Added validation to profile-view component

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -20,7 +20,8 @@ export class ProfileView extends React.Component {
       Email: null,
       Birthday: null,
       Password: null,
-      FavoriteMovies: []
+      FavoriteMovies: [],
+      errorText: {}
     };
   }
 
@@ -42,6 +43,12 @@ export class ProfileView extends React.Component {
 
   setPassword(input) {
     this.Password = input;
+  }
+
+  setErrorText(errors) {
+    this.setState({
+      errorText: errors
+    });
   }
 
   getUserInfo() {
@@ -71,29 +78,30 @@ export class ProfileView extends React.Component {
     e.preventDefault();
     const token = localStorage.getItem('token');
     const userName = localStorage.getItem('user');
+    const isValid = this.formValidation(this);
 
-    console.log(this.state)
-
-    axios.put(`https://movie-seek-1949.herokuapp.com/users/${userName}`, {
-      Username: this.Username ? this.Username : this.state.Username,
-      Password: this.Password ? this.Password : this.state.Password,
-      Email: this.Email ? this.Email : this.state.Email,
-      Birthdate: this.Birthday ? this.Birthday : this.state.Birthday
-    }, {
-      headers: { Authorization: `Bearer ${token}`}
-    })
-    .then(response => {
-      const data = response.data;
-      this.setState({
-        Username: data.Username,
-        Email: data.Email,
-        Birthday: data.Birthday,
-        Password: data.Password
+    if (isValid) {
+      axios.put(`https://movie-seek-1949.herokuapp.com/users/${userName}`, {
+        Username: this.Username ? this.Username : this.state.Username,
+        Password: this.Password ? this.Password : this.state.Password,
+        Email: this.Email ? this.Email : this.state.Email,
+        Birthdate: this.Birthday ? this.Birthday : this.state.Birthday
+      }, {
+        headers: { Authorization: `Bearer ${token}`}
+      })
+      .then(response => {
+        const data = response.data;
+        this.setState({
+          Username: data.Username,
+          Email: data.Email,
+          Birthday: data.Birthday,
+          Password: data.Password
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
+    }
   }
 
   deleteProfile(e) {
@@ -132,9 +140,32 @@ export class ProfileView extends React.Component {
     });
   }
 
+  formValidation(form) {
+    let isValid = true;
+    let formErrors = {};
+
+    if (form.Username === '') {
+      formErrors.username = 'Username is required';
+      isValid = false;
+    }
+
+    if (form.Password === '') {
+      formErrors.password = 'Password is required';
+      isValid = false;
+    }
+
+    if (form.Email === '') {
+      formErrors.email = 'Email is required';
+      isValid = false;
+    }
+
+    setErrorText(formErrors);
+    return isValid;
+  }
+
   render() {
     const { movies } = this.props;
-    const { FavoriteMovies } = this.state;
+    const { FavoriteMovies, errorText } = this.state;
 
     return (
       <Row>
@@ -144,23 +175,41 @@ export class ProfileView extends React.Component {
           {/* Form to update profile information */}
           <Form onSubmit={e => this.handleUpdate(e)}>
             <Form.Group>
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" onChange={e => this.setUsername(e.target.value)} />
+              <Form.Label>Username*</Form.Label>
+              <Form.Control 
+                required 
+                type="text" 
+                onChange={e => this.setUsername(e.target.value)} 
+              />
+              { (errorText && 'username' in errorText) ? <p className="text-danger">{errorText['username']}</p> : '' }
             </Form.Group>
 
             <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" onChange={e => this.setEmail(e.target.value)} />
+              <Form.Label>Email*</Form.Label>
+              <Form.Control 
+                required 
+                type="email" 
+                onChange={e => this.setEmail(e.target.value)} 
+              />
+              { (errorText && 'email' in errorText) ? <p className="text-danger">{errorText['email']}</p> : '' }
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Password*</Form.Label>
+              <Form.Control 
+                required 
+                type="password" 
+                onChange={e => this.setPassword(e.target.value)} 
+              />
+              { (errorText && 'password' in errorText) ? <p className="text-danger">{errorText['password']}</p> : '' }
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Birthday</Form.Label>
-              <Form.Control type="date" onChange={e => this.setBirthday(e.target.value)} />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" onChange={e => this.setPassword(e.target.value)} />
+              <Form.Control 
+                type="date" 
+                onChange={e => this.setBirthday(e.target.value)} 
+              />
             </Form.Group>
 
             <Button variant="primary" type="submit">Update</Button>
