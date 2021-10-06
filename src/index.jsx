9a8689a437314995dfaf14dc976998f43e1f7874
Alex Guizar <alexguizar90@gmail.com>
2021-10-05 20:20:32 -0700

    Added redux packages to the index.jsx file. Also added the store and provider wrapper in the index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,33 @@
+// Imported packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
+import moviesApp from './reducers/reducers';
+// Imported Custom Components
 import MainView from './components/main-view/main-view';
 import { HeaderNav } from './components/header-nav/header-nav';
 
+// Imported React-Bootstrap Components
 import Container from 'react-bootstrap/Container';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MovieSeekApplication extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <Provider store={store}>
         <HeaderNav />
         <Container className="py-3">
           <MainView />
         </Container>
-      </React.Fragment>
+      </Provider>
     );
   }
 }
